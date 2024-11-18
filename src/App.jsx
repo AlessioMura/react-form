a863c8abffb7e3472264ec0ca3165399c27be700
AlessioMura <alessiomura03@gmail.com>
2024-11-18 16:42:31 +0100

    added markup

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,22 @@ function App() {
 
   return (
     <>
+      <div className="container">
+        <h1 className='py-4'>Blog posts</h1>
+
+        <form>
+
+          <div>
+            <div className="input-group mb-3">
+              <input type="text" className="form-control" placeholder="Blog Title" aria-label="Blog Title" aria-describedby="button-addon2" />
+                <button className="btn btn-outline-secondary" type="submit" id="button-addon2">Submit</button>
+            </div>
+
+          </div>
+
+        </form>
+
+      </div>
     </>
   )
 }
